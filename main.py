54d79e38db3b7550fdadb4d0034c18630352d961
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-05-28 12:24:05 +0530

    be code added with db connection object and cors middleware and all methods fastapi code

--- main.py
+++ main.py
@@ -63,12 +63,12 @@ def add_expense(payload: dict):
     """
 
     values = (
-        data["title"],
-        data["amount"],
-        data["category"],
-        data["payment_method"],
-        data["expense_date"],
-        data["description"]
+        payload["title"],
+        payload["amount"],
+        payload["category"],
+        payload["payment_method"],
+        payload["expense_date"],
+        payload["description"]
     )
 
     cursor.execute(query, values)
@@ -141,12 +141,12 @@ def update_expense(expense_id: int, payload: dict):
     """
 
     values = (
-        data["title"],
-        data["amount"],
-        data["category"],
-        data["payment_method"],
-        data["expense_date"],
-        data["description"],
+        payload["title"],
+        payload["amount"],
+        payload["category"],
+        payload["payment_method"],
+        payload["expense_date"],
+        payload["description"],
         expense_id
     )
 
